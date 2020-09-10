bb57a6c801bed5f49b724335711a8b074b6872ab
dawernow69 <42340452+dawernow69@users.noreply.github.com>
2020-09-10 10:36:41 +0200

    Added files fetch

---
+++ Webb/fetch/script.js
@@ -0,0 +1,49 @@
+let i;
+let paragraph;
+let section;
+let text;
+
+let myJSON = '{"name":"Tim Tuvestam","age":28}';
+let myObj = JSON.parse(myJSON);
+
+fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+  .then(response => alert (response.json()))
+  .then(commits => alert(commits[0].author.login));
+
+
+function init(){
+    
+    document.getElementById("btn").onclick = stycke;
+    start();
+}
+window.onload = init;
+
+function stycke(){
+    text = document.createTextNode("stycke");
+    paragraph = document.createElement("p");    
+    paragraph.appendChild(text);
+    document.getElementById("text").appendChild(paragraph);
+}
+
+function sections(){
+    section = document.createElement("section");
+    section.setAttribute("id","newSection");
+    document.getElementById("main").appendChild(section);
+    text = document.createTextNode(myObj.name);
+    paragraph = document.createElement("p");    
+    paragraph.appendChild(text);
+    document.getElementById("newSection").appendChild(paragraph)
+    text = document.createTextNode(myObj.age);
+    paragraph = document.createElement("p");    
+    paragraph.appendChild(text);
+    document.getElementById("newSection").appendChild(paragraph)
+}
+
+function start(){
+    for(i=0;i<2;i++){
+        stycke();
+    }
+
+    sections();
+}
+
