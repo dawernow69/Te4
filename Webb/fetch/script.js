df572cfdba513356c09464994e4e70f5f3a11a8e
dawernow69 <david.wernow@vaxjo.se>
2020-09-17 13:52:30 +0200

    Update script.js

--- Webb/fetch/script.js
+++ Webb/fetch/script.js
@@ -2,32 +2,76 @@ let i;
 let paragraph;
 let section;
 let text;
+let data;
 
 let myJSON = '{"name":"Tim Tuvestam","age":28}';
 let myObj = JSON.parse(myJSON);
 
-function init(){    
+fetch('http://94.46.140.3:8080/teknikumMenu/api/menu').then(response => {
+    return response.json()
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+    let meals=data.meals;
+    let week=data.week;
+    let i=0;
+    console.log(week + " " + meals[0].dish);
+    while(meals){
+      sections(meals[i]);
+      i++;
+    }
+    //return data;
+  }).catch(err => {
+    // Do something for an error here
+  });
+ 
+/*function jsonObj(){
+  let newObject;
+  fetch('http://94.46.140.3:8080/teknikumMenu/api/menu')
+    .then(response => console.log(response.text())
+    .then(text => console.log(text))
+      // Work with JSON data here
+    //console.log(text);
+    //newObj=JSON.parse(data);
+    //alert("NewOject = " + newObj);
+    //return (data);
+    //.catch(err => );
+    // Do something for an error her
+  
+  
+
+  
+}*/
+
+
+
+function init(){
+    
     document.getElementById("btn").onclick = stycke;
-    start();
+    //jsonObj();
+    
+    //start();
+    
+   
 }
 window.onload = init;
 
 function stycke(){
     text = document.createTextNode("stycke");
-    paragraph = document.createElement("p");
+    paragraph = document.createElement("p");    
     paragraph.appendChild(text);
     document.getElementById("text").appendChild(paragraph);
 }
 
-function sections(){
+function sections(dish){
     section = document.createElement("section");
     section.setAttribute("id","newSection");
     document.getElementById("main").appendChild(section);
-    text = document.createTextNode(myObj.name);
+    text = document.createTextNode(dish.dish);
     paragraph = document.createElement("p");    
     paragraph.appendChild(text);
     document.getElementById("newSection").appendChild(paragraph)
-    text = document.createTextNode(myObj.age);
+    text = document.createTextNode(dish.alt_dish);
     paragraph = document.createElement("p");    
     paragraph.appendChild(text);
     document.getElementById("newSection").appendChild(paragraph)
