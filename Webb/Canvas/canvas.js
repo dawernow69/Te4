54134e1b520428bb89c1d76071eaf555cb423882
dawernow69 <42340452+dawernow69@users.noreply.github.com>
2021-08-17 09:13:36 +0200

    Nya filer

--- Webb/Canvas/canvas.js
+++ Webb/Canvas/canvas.js
@@ -2,19 +2,17 @@ var canvas = document.getElementById("myCanvas");
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
     ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
+    ctx.lineTo(200, 200);
     ctx.stroke();
 
 
     //ctx.fillRect(25, 25, 100, 100);
     //ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
+    ctx.strokeRect(50, 0, 50, 50);
 
     ctx.beginPath();
     ctx.arc(150, 120, 20, 0, 2 * Math.PI);
     ctx.stroke();
 
   }
-  else{
-      canvas.innerHTML="<p>Saknar stöd för canvas</p>";
-  }
+ 
---
+++ Webb/boklista/script.js
@@ -0,0 +1,10 @@
+books = {book:[
+    {Author:"Tim Tuvestam",Title:"Börja med JAVA"},
+    {Author:"Lars Gunther",Title:"Webbutveckling 1"}
+]}
+
+document.getElementById("author").innerHTML=books.book[0].Author;
+document.getElementById("title").innerHTML=books.book[0].Title;
+
+document.getElementById("author2").innerHTML=books.book[1].Author;
+document.getElementById("title2").innerHTML=books.book[1].Title;
