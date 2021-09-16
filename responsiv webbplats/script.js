f938b5c4b81dad36b07da8cdeec4ae84a1718e3b
dawernow69 <42340452+dawernow69@users.noreply.github.com>
2021-09-16 16:36:57 +0200

    updated files

--- responsiv webbplats/script.js
+++ responsiv webbplats/script.js
@@ -3,6 +3,10 @@ let images = [
     {"imageID":1,src:"./img/bild1.png",date:"2021-09-14",title:"En bild",user:"Tim Tuvestam"},
     {"imageID":2,src:"./img/bild2.png",date:"2021-09-14",title:"En annan bild",user:"Tim Tuvestam"},
     {"imageID":3,src:"./img/bild3.png",date:"2021-09-14",title:"Ytterligare en bild",user:"David Wernow"},
+    {"imageID":4,src:"./img/bild4.png",date:"2021-09-14",title:"En sista bild",user:"David Wernow"},
+    {"imageID":1,src:"./img/bild1.png",date:"2021-09-14",title:"En bild",user:"Tim Tuvestam"},
+    {"imageID":2,src:"./img/bild2.png",date:"2021-09-14",title:"En annan bild",user:"Tim Tuvestam"},
+    {"imageID":3,src:"./img/bild3.png",date:"2021-09-14",title:"Ytterligare en bild",user:"David Wernow"},
     {"imageID":4,src:"./img/bild4.png",date:"2021-09-14",title:"En sista bild",user:"David Wernow"}
 ]
 
@@ -11,43 +15,44 @@ function init(){
     click = document.getElementById("klicka");
     let counter=0;
     let len = images.length;
-    console.log(len);
 
     images.forEach(element => {
         counter++;
-        createSection(counter);
+        createArticle(counter);
+       
     });
 
-    click.addEventListener("click",event=>{
+    /*click.addEventListener("click",event=>{
         klicka();
         event.preventDefault();       
-    });
+    });*/
 }
 window.onload = init;
 
-function klicka(){
-    console.log("hej");
+function klicka(article){
+    console.log("hej" + article);
 }
 
-function createSection(counter){
-    let section = document.createElement("section");
-    let sectionClass = "s"+counter;
-    console.log(counter,sectionClass);
-    section.id = sectionClass;
-    click.appendChild(section);
-    createFigure(counter, sectionClass);
-
+function createArticle(counter){
+    let article = document.createElement("article");
+    let articleId = "s"+counter;
+    article.id = articleId;
+    click.appendChild(article);
+    createFigure(counter, articleId);
+    article.addEventListener("click",event=>{
+        klicka(articleId);
+        event.preventDefault();
+    });
 }
 
-function createImage(counter, figureClass){
+function createImage(counter, figureId){
     let img = document.createElement("img");
-    let figure = document.getElementById(figureClass);
-    console.log(counter,figureClass, figure);
+    let figure = document.getElementById(figureId);
     let figCaption = document.createElement("figcaption");
    
   
     img.src = images[counter-1].src;
-    text = images[counter-1].user + " " + images[counter-1].title;
+    text = images[counter-1].user + "      " + images[counter-1].title;
     figCaption.innerHTML = text;
     
     figure.appendChild(img);
@@ -55,17 +60,16 @@ function createImage(counter, figureClass){
 
 }
 
-function createFigure(counter, sectionClass){
+function createFigure(counter, articleId){
     let figure = document.createElement("figure");
     
     let text; 
-    let section = document.getElementById(sectionClass);
-    let figureClass = "f"+counter;
-    figure.id = figureClass;
-    console.log(counter,figureClass);
+    let article = document.getElementById(articleId);
+    let figureId = "f"+counter;
+    figure.id = figureId;
     
-    section.appendChild(figure);
-    createImage(counter, figureClass);
+    article.appendChild(figure);
+    createImage(counter, figureId);
 
 
 }
