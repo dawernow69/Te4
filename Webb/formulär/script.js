05df25b1bd78db50f336569757d6fb7be944ba19
dawernow69 <42340452+dawernow69@users.noreply.github.com>
2022-03-07 20:29:29 +0100

    Formuläruppgift

--- Webb/formulär/script.js
+++ Webb/formulär/script.js
@@ -1,22 +1,27 @@
 let form = document.querySelector("form");
 let fileElem = document.getElementById("file");
+let imgElem = form.querySelector("img");
+var file;
 let object;
 
 form.addEventListener("submit", event => {
     let namn= form.elements.namn.value;
     let klass = form.elements.klass.value;
+    console.log(file);
     object = {
         "name":namn,
-        "klass":klass
+        "klass":klass,
+        "filnamn":file
     };
     
     console.log(JSON.stringify(object));
+    URL.revokeObjectURL(imgElem.src);
     event.preventDefault();
 });
 
 fileElem.addEventListener("change",()=>{
     if(fileElem.files.length > 0){
-        let file = fileElem.files[0];
+        file = fileElem.files[0];
         console.log("Filename " + file);
         if(file.type) console.log("Filetype: " + file.type);
 
@@ -24,15 +29,19 @@ fileElem.addEventListener("change",()=>{
 
         console.log(reader);
         reader.readAsDataURL(file);
-        reader.addEventListener("loadend", ()=>{
+        reader.addEventListener("loadend", event=>{
             console.log("File: " + reader.result);
+            file = reader.result;
             const base64String = reader.result
             .replace("data:", "")
             .replace(/^.+,/, "");
 
             console.log("File in base64: "+base64String);
-        });         
-    
+            event.preventDefault();
+        });
+        let url = URL.createObjectURL(file);
+        console.log("URL: " + url);
+        imgElem.src = url;
     }
 });
 
