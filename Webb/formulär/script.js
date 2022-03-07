4294a2ce01237782f97687a89f2653fe2a28b1dd
dawernow69 <42340452+dawernow69@users.noreply.github.com>
2022-03-07 17:47:48 +0100

    Filehandling

--- Webb/formulär/script.js
+++ Webb/formulär/script.js
@@ -1,4 +1,5 @@
 let form = document.querySelector("form");
+let fileElem = document.getElementById("file");
 let object;
 
 form.addEventListener("submit", event => {
@@ -11,5 +12,27 @@ form.addEventListener("submit", event => {
     
     console.log(JSON.stringify(object));
     event.preventDefault();
+});
+
+fileElem.addEventListener("change",()=>{
+    if(fileElem.files.length > 0){
+        let file = fileElem.files[0];
+        console.log("Filename " + file);
+        if(file.type) console.log("Filetype: " + file.type);
+
+        let reader = new FileReader();
+
+        console.log(reader);
+        reader.readAsDataURL(file);
+        reader.addEventListener("loadend", ()=>{
+            console.log("File: " + reader.result);
+            const base64String = reader.result
+            .replace("data:", "")
+            .replace(/^.+,/, "");
+
+            console.log("File in base64: "+base64String);
+        });         
+    
+    }
 });
 
