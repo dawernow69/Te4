9f4cc25a6df01ffe47ea71a3d91ebce8a3fcaa4a
dawernow69 <42340452+dawernow69@users.noreply.github.com>
2023-08-23 17:02:10 +0200

    Updated script.js

--- Webb/booklist/script.js
+++ Webb/booklist/script.js
@@ -1,4 +1,4 @@
-let myJSON = '{"title":"Programmering med Visual Basic","author":"Tim Tuvestam"}';
+/*let myJSON = '{"title":"Programmering med Visual Basic","author":"Tim Tuvestam"}';
 let myObj = JSON.parse(myJSON);
 
 books = {book:[
@@ -11,3 +11,34 @@ document.getElementById("author").innerHTML = books.book[0].Author;
 
 document.getElementById("title1").innerHTML = books.book[1].Title;
 document.getElementById("author1").innerHTML = books.book[1].Author;
+*/
+
+let author;
+let author1;
+let title;
+let title1;
+
+function init(){
+    author = document.getElementById("author");
+    author1 = document.getElementById("author1");
+    title = document.getElementById("title");
+    title1 = document.getElementById("title1");
+
+    fetchBooks();
+}
+
+window.onload = init;
+
+async function fetchBooks(){
+    let path = "https://its.te4.nu/~david/test";
+    let response = await fetch(path);
+
+    console.log(response);
+
+    let json = response.json();
+
+    console.log(json);
+    
+    author.innerHTML = json.author;
+    title.innerHTML = json.title;
+}
